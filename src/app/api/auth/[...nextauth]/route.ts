972c0a2739231e5241ca7888d6cccc9a70f61078
Jari Jokinen <jari.jokinen@j2.fi>
2023-07-20 22:48:02 +0300

    Add Prisma

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,21 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from '@/lib/prisma';
 
 import type { NextAuthOptions } from 'next-auth';
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string
     })
-  ]
+  ],
+  session: {
+    strategy: 'jwt'
+  }
 };
 
 const handler = NextAuth(authOptions);
