73ae74991c00cde4a86c2017318cb0a281a93d1a
Jari Jokinen <jari.jokinen@j2.fi>
2023-07-20 03:40:44 +0300

    Add hamburger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,30 @@
+'use client';
+
+import styles from './Header.module.scss';
+
+import { useState } from 'react';
 import Link from 'next/link';
 
 export default function Header() {
+  const [navOn, setNavOn] = useState(false);
+  const navClasses = [styles.nav, navOn ? styles.navOn : ''].join(' ');
+
   return (
-    <header>
+    <header className={styles.header}>
       <div className="container">
-        <div>
+        <div className={styles.logo}>
           <Link href="/">Next.js Starter</Link>
         </div>
-        <nav>
+        <nav className={navClasses}>
           <ul>
             <li>
               <Link href="/">Home</Link>
             </li>
           </ul>
+          <div
+            className={styles.navToggle}
+            onClick={() => setNavOn(!navOn)}
+          ></div>
         </nav>
       </div>
     </header>
